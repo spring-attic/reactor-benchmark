bf08aad929859f9be4e39ed27dfa4e595c0456a6
Stephane Maldini <smaldini@gopivotal.com>
2014-08-15 03:51:56 +0100

    Add Streams.parallel for MPMS scenarios ( no intermediate Stream between producers and subscribers )

--- src/main/java/org/projectreactor/bench/composable/StreamBenchmarks.java
+++ src/main/java/org/projectreactor/bench/composable/StreamBenchmarks.java
@@ -30,7 +30,7 @@ import java.util.concurrent.TimeUnit;
  * @author Jon Brisbin
  * @author Stephane Maldini
  */
-@Measurement(iterations = StreamBenchmarks.ITERATIONS, time = 100)
+@Measurement(iterations = StreamBenchmarks.ITERATIONS, time = 5)
 @Warmup(iterations = 0)
 @Fork(1)
 @BenchmarkMode(Mode.Throughput)
@@ -117,7 +117,7 @@ public class StreamBenchmarks {
 		for (int i : data) {
 			mapManydeferred.broadcastNext(i);
 		}
-		if(!latch.await(60, TimeUnit.SECONDS)) throw new RuntimeException(mapManydeferred.debug().toString());
+		if(!latch.await(30, TimeUnit.SECONDS)) throw new RuntimeException(mapManydeferred.debug().toString());
 	}
 
 }
